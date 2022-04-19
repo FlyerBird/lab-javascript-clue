56c8f687e2f30125cd45718a6e388efb1bb2203b
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-04-19 17:38:20 +0200

    done

--- src/clue.js
+++ src/clue.js
@@ -1,231 +1,195 @@
 // ITERATION 1
-//SUSPECTS OBJECTS
-
-let mrGreen = {
-  firstName: "Jacob",
-  lastName: "Green",
-  occupation: "Entrepreneur",
-  age: 45,
-  description: "He has a lot of connections",
-  image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-  color: "green"
-};
-
-let drOrchid =  {
-  firstName: "Doctor",
-  lastName: "Orchid",
-  occupation: "Scientist",
-  age: 26,
-  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-  image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-  color: "white"
-};
-
-let profPlum =   {
-  firstName: "Victor",
-  lastName: "Plum",
-  occupation: "Designer",
-  age: 22,
-  description: "Billionaire video game designer",
-  image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-  color: "purple"
-};
-
-let missScarlet =  {
-  firstName: "Kasandra",
-  lastName: "Scarlet",
-  occupation: "Actor",
-  age: 31,
-  description: "She is an A-list movie star with a dark past",
-  image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
-  color: "red"
-};
-
-let mrsPeacock =   {
-  firstName: "Eleanor",
-  lastName: "Peacock",
-  occupation: "Socialité",
-  age: 36,
-  description: "She is from a wealthy family and uses her status and money to earn popularity",
-  image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-  color: "blue"
-};
-
-let mrMustard = {
-  firstName: "Jack",
-  lastName: "Mustard",
-  occupation: "Retired Football player",
-  age: 62,
-  description: "He is a former football player who tries to get by on his former glory",
-  image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-  color: "yellow"
-};
-
-
-//ROOM OBJECTS
-let diningRoom =   {
-  name: "Dining Room"
-};
-
-let conservatory =  {
-  name: "Conservatory"
-};
-
-let kitchen =  {
-  name: "Kitchen"
-};
-
-let study =  {
-  name: "Study"
-};
-
-let library =   {
-  name: "Library"
-};
-
-let billiardRoom = {
-  name: "Billiard Room"
-};
-
-let lounge =  {
-  name: "Lounge"
-};
-
-let ballRoom =   {
-  name: "Ballroom"
-};
-
-let hall = {
-  name: "Hall"
-};
-
-let spa =  {
-  name: "Spa"
-};
-
-let livingRoom = {
-  name: "Living Room"
-};
-
-let observatory =   {
-  name: "Observatory"
-};
-
-let theater =  {
-  name: "Theater"
-};
-
-let guestHouse =  {
-  name: "Guest House"
-};
-
-let patio =   {
-  name: "Patio"
-};
-
-
-//WEAPONS OBJECTS
-let rope =  {
-  name: "rope",
-  weight: 10
-};
-
-let knife =   { 
-  name: "knife",
-  weight: 8
-};
-
-let candlestick =   {
-  name: "candlestick",
-  weight: 2
-};
-
-let dumbbell = {
-  name: "dumbbell",
-  weight: 30
-};
-
-let poison =   {
-  name: "poison",
-  weight: 2
-};
-
-let axe =   {
-  name: "axe",
-  weight: 15
-};
-
-let bat =   {
-  name: "bat",
-  weight: 13
-};
-
-let trophy =   {
-  name: "trophy",
-  weight: 25
-};
-
-let pistol = {
-  name: "pistol",
-  weight: 20
-};
-
 
 // Suspects Array
-const suspectsArray = [
-    mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard
-  ];
 
-// Rooms Array
-const roomsArray = [
-    diningRoom, conservatory, kitchen, study, library, billiardRoom, lounge, ballRoom, hall, spa, livingRoom, observatory, theater, guestHouse, patio
-  ];
+// const suspectsArray = [
+//   "mrGreen", "drOrchid", "profPlum", "missScarlet", "mrsPeacock", "mrMustard"
+// ];
+
+const suspectsArray = [
+  {
+      firstName: "Jacob",
+      lastName: "Green",
+      occupation: "Entrepreneur",
+      age: 45,
+      description: "He has a lot of connections",
+      image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+      color: "green"
+  },
+    {
+    firstName: "Doctor",
+    lastName: "Orchid",
+    occupation: "Scientist",
+    age: 26,
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: "white",
+  },
+  {
+    firstName: "Victor",
+    lastName: "Plum",
+    occupation: "Designer",
+    age: 22,
+    description: "Billionaire video game designer",
+    image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+    color: "purple"
+  },
+  {
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    occupation: "Actor",
+    age: 31,
+    description: "She is an A-list movie star with a dark past",
+    image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: "red"
+  },
+  {
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    occupation: "Socialité",
+    age: 36,
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    color: "blue"
+  },
+  {
+    firstName: "Jack",
+    lastName: "Mustard",
+    occupation: "Retired Football player",
+    age: 62,
+    description: "He is a former football player who tries to get by on his former glory",
+    image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+    color: "yellow"
+  }
+]
 
 // Weapons Array
+
+// const weaponsArray = [
+//   "rope", "knife", "candlestick", "dumbbell", "poison", "axe", "bat", "trophy", "pistol"
+// ];
 const weaponsArray = [
-    rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol
-  ];
+
+  {
+    name: "rope",
+    weight: 10
+  },
+  {
+    name: "knife",
+    weight: 8
+  },
+  {
+    name: "candlestick",
+    weight: 2
+  },
+  {
+    name: "dumbbell",
+    weight: 30
+  },
+  {
+    name: "poison",
+    weight: 2
+  },
+  {
+    name: "axe",
+    weight: 15
+  },
+  {
+    name: "bat",
+    weight: 13
+  },
+  {
+    name: "trophy",
+    weight: 25
+  },
+  {
+    name: "pistol",
+    weight: 20
+  }
+  ]
+
+// Rooms Array
+
+// const roomsArray = [
+//   "diningRoom", "conservatory", "kitchen", "study", "library", "billiardRoom", "lounge", "ballRoom", "hall", "spa", "livingRoom", "observatory", "theater", "guestHouse", "patio"
+// ];
+const roomsArray = [
+  {
+  name: "Dining Room"
+  },
+  {
+  name: "Conservatory"
+  },
+  {
+  name: "Kitchen"
+  },
+  {
+  name: "Study"
+  },
+
+  {
+  name: "Library"
+  },
+  {
+  name: "Billiard Room"
+  },
+  {
+  name: "Lounge"
+  },
+  {
+  name: "Ballroom"
+  },
+  {
+  name: "Hall"
+  },
+  {
+  name: "Spa"
+  },
+  {
+  name: "Living Room"
+  },
+  {
+  name: "Observatory"
+  },
+  {
+  name: "Theater"
+  },
+  {
+  name: "Guest House"
+  },
+  {
+  name: "Patio"
+  }
+  ]
 
 
 // ITERATION 2
 
-
-
-function selectRandom() {}
+function selectRandom(arr) {
+  return arr[Math.floor(Math.random()*arr.length)]
+}
+// console.log(selectRandom(suspectsArray));
+// console.log(selectRandom(weaponsArray));
+// console.log(selectRandom(roomsArray));
 
 function pickMystery() {
-  let randomSupect = suspectsArray[Math.floor(Math.random()*suspectsArray.length)]
-  let randomRoom = roomsArray[Math.floor(Math.random()*roomsArray.length)]
-  let randomWeapon = weaponsArray[Math.floor(Math.random()*weaponsArray.length)]
-
-  return {randomSupect, randomRoom, randomWeapon}
+  return suspectObject = {
+    suspect: selectRandom(suspectsArray),
+    weapon: selectRandom(weaponsArray),
+    room: selectRandom(roomsArray),
 }
-
+}
+//console.log(pickMystery())
 
 
 
 
 // ITERATION 3
 
-let revealMistery = pickMystery()
-
-document.querySelector("button").onclick = guess;
-
-function guess(){
-   let guessedSuspect = document.querySelector("#suspect").value
-   let guessedWeapon = document.querySelector("#weapon").value
-   let guessedRoom = document.querySelector("#room").value
-
-   if (guessedSuspect === revealMistery.suspect.name 
-   && guessedWeapon === revealMistery.weapon.name 
-   && guessedRoom === revealMistery.room.name) {
-   alert ("Yes!!! You guessed right!!!")
-}  else {
-   alert ("Nope, you guessed wrong.")
-  
+function revealMystery(obj) {
+  let resolved = pickMystery()
+  return `${resolved.suspect.firstName} ${resolved.suspect.lastName} killed Mr.Boddy using the ${resolved.weapon.name} in the ${resolved.room.name}`
 }
-}
-
 
 
 
